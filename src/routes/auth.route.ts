73ca4c2798f30ede61424c8885c45547653b41a3
Nguyen Vu <85710694+avno777@users.noreply.github.com>
2024-08-16 21:41:30 +0700

    add rate limit

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,19 +1,20 @@
 import express, { Router } from 'express'
 import authController from '../controllers/auth.controller'
 //import { authMiddleware, authorizeRoles } from '../middlewares/auth.middleware'
+import authLimiter from '~/middlewares/rateLimit.middleware'
 
 const router: Router = express.Router()
 
-router.post('/register', authController.registerController)
-router.post('/login', authController.loginController)
-router.post('/active', authController.active)
-router.post('/refresh-token', authController.refreshTokenController)
-router.post('/send-otp-forgot', authController.sendOtpForGot)
-router.post('/verify-otp-forGot', authController.verifyOtpForGot)
-router.post('/change-password', authController.changePasswordController)
-router.post('/reset-password', authController.changePasswordController)
-router.post('/logout', authController.logoutController)
-router.get('/google', authController.googleLogin)
-router.get('/google/callback', authController.googleCallback)
+router.post('/register', authLimiter, authController.registerController)
+router.post('/login', authLimiter, authController.loginController)
+router.post('/active', authLimiter, authController.active)
+router.post('/refresh-token', authLimiter, authController.refreshTokenController)
+router.post('/send-otp-forgot', authLimiter, authController.sendOtpForGot)
+router.post('/verify-otp-forGot', authLimiter, authController.verifyOtpForGot)
+router.post('/change-password', authLimiter, authController.changePasswordController)
+router.post('/reset-password', authLimiter, authController.changePasswordController)
+router.post('/logout', authLimiter, authController.logoutController)
+router.get('/google', authLimiter, authController.googleLogin)
+router.get('/google/callback', authLimiter, authController.googleCallback)
 
 export default router
